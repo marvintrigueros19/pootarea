a0b374d15b24291711e2c1306c4b23af7566fb42
Marvin Trigueros <Marvin Trigueros @192.168.16.40>
2019-03-18 15:06:07 -0600

    Primer commit para repositorio de tararea =

---
+++ src/tareapoogr/persona.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tareapoogr;
+
+/**
+ *
+ * @author Marvin Trigueros
+ */
+public class persona {
+    
+    
+     //atributos
+    private String nombre; 
+    public String profesion; 
+    
+    public persona() {
+        //constructor
+    }
+    
+    // establecer y obtener
+    
+    public void establecerNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String obtenerNombre(){
+        return this.nombre;
+        
+    }
+    
+    // establecer y obtener
+    
+    public void establecerProfesion(String profesion){
+        this.nombre = profesion;
+    }
+    
+    public String obtenerProfesion(){
+        return this.profesion;
+        
+    }
+    
+    
+    
+}
